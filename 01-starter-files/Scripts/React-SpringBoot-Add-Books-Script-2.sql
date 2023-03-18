b35c4c0b829d2611cefb60432311781412d5f418
klalit57 <97937345+klalit57@users.noreply.github.com>
2023-03-18 13:26:58 +0530

    Add files via upload

---
+++ 01-starter-files/Scripts/React-SpringBoot-Add-Books-Script-2.sql
@@ -0,0 +1,176 @@
+( 1,"JavaScript Cookbook","Luv, Zofia","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tortor, condimentum eget sapien ac, dapibus varius ligula. Maecenas justo erat, semper sed nunc vel, vulputate eleifend dui. Integer id ipsum vitae nisi malesuada feugiat. Proin sit amet quam laoreet, feugiat mi vitae, vestibulum dui. Aliquam erat volutpat. Etiam hendrerit erat nec mi auctor elementum. Curabitur vestibulum lectus a ante tempor tincidunt et sed orci. Proin maximus tortor in risus auctor efficitur. Phasellus quam mauris, laoreet et feugiat ac, imperdiet at quam. Nullam sollicitudin nec diam vel finibus.",10,10,"FE","JavaScript Cookbook"};
+( 2,"Become a Guru in JavaScript","Luv, Lena","Pellentesque varius aliquam lacus quis rhoncus. Nam a dui lectus. Vestibulum libero elit, ultricies sit amet sagittis eu, molestie at velit. Donec tincidunt tempus magna, quis facilisis libero elementum non. Sed velit lacus, laoreet sed augue fermentum, sagittis convallis metus. Sed nec est at massa venenatis aliquet. Donec pretium interdum fringilla. Sed ornare tellus enim, a tincidunt libero dictum vitae. Proin bibendum posuere dui. Donec sagittis neque massa, sed semper nulla vehicula at.",15,15,"FE","Become a Guru in JavaScript"};
+( 3,"Exploring Vue.js","Luv, Jakub","Proin at urna faucibus, pretium mi in, dapibus metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam elementum ligula ut vestibulum tincidunt. Aenean dignissim enim eu libero accumsan, ultricies efficitur mauris bibendum. Sed vel blandit ligula, non tincidunt velit. Suspendisse vulputate tristique enim, a vulputate arcu accumsan vitae. Donec egestas ornare velit vestibulum tincidunt. Suspendisse ac dui nec tellus vulputate aliquet. Curabitur vitae mauris in turpis vulputate hendrerit at eget urna. Donec at suscipit nisi. Ut sit amet dignissim orci. Nunc sit amet tristique enim. Phasellus consectetur auctor dignissim. Curabitur cursus turpis dapibus, molestie dui finibus, molestie ante. Proin quis justo ut ipsum aliquet venenatis in ut enim.",10,10,"FE","img");
+( 4,"Advanced Techniques in Big Data","Luv, Alexander","Nunc eget lorem ac neque tincidunt mollis. Fusce finibus laoreet nunc nec hendrerit. Curabitur eu placerat urna, sit amet pellentesque enim. Donec velit ligula, congue eu lobortis vel, interdum nec tellus. Nulla nisl ipsum, porta non egestas sed, vulputate quis nisi. Etiam pellentesque in velit non convallis. Nullam id risus quis augue posuere sodales vel maximus justo. Maecenas nec leo a nibh aliquet placerat nec sed massa. Duis sit amet nisi libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non viverra dolor. Pellentesque ligula mauris, congue quis neque quis, mollis scelerisque ligula. Pellentesque semper, erat commodo luctus mollis, nulla ipsum consectetur dolor, quis blandit massa sem fringilla libero. Maecenas eget mi nec est condimentum fermentum. Vivamus vehicula est sit amet ante gravida, eu finibus quam elementum. Proin egestas leo eu sagittis euismod.",12,12,"Data","img"};
+( 5,"Crash Course in Big Data","Luv, Judy","Morbi eu tempus eros, in imperdiet sem. Nulla sed sagittis nisl, porttitor fringilla libero. Nullam ut urna aliquet, hendrerit quam in, dignissim diam. In in nibh vel nisi fermentum pretium sit amet vitae mi. Pellentesque eget augue efficitur, volutpat tellus eget, fringilla augue. Pellentesque tempus mi ac risus lacinia, et tincidunt lectus rutrum. Nullam et nibh a odio luctus tincidunt nec in ipsum. Sed ac est nulla. Nulla purus turpis, dignissim sit amet euismod lobortis, consequat ut dui. Maecenas commodo velit in elementum placerat. Nam sit amet blandit ante, sit amet mollis neque. Ut placerat venenatis leo sit amet dapibus. Nunc varius cursus lobortis. Aenean euismod dui at diam euismod aliquet. Fusce feugiat orci nec commodo placerat.",20,20,"Data","img"};
+	{
+		"id" : 6,
+		"title" : "Beginners Guide to SQL",
+		"author" : "Luv, Anup",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tortor, condimentum eget sapien ac, dapibus varius ligula. Maecenas justo erat, semper sed nunc vel, vulputate eleifend dui. Integer id ipsum vitae nisi malesuada feugiat. Proin sit amet quam laoreet, feugiat mi vitae, vestibulum dui. Aliquam erat volutpat. Etiam hendrerit erat nec mi auctor elementum. Curabitur vestibulum lectus a ante tempor tincidunt et sed orci. Proin maximus tortor in risus auctor efficitur. Phasellus quam mauris, laoreet et feugiat ac, imperdiet at quam. Nullam sollicitudin nec diam vel finibus.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "Data",
+		"img" : ?
+	},
+	{
+		"id" : 7,
+		"title" : "Advanced Techniques in JavaScript",
+		"author" : "Luv, Yasemin",
+		"description" : "Pellentesque varius aliquam lacus quis rhoncus. Nam a dui lectus. Vestibulum libero elit, ultricies sit amet sagittis eu, molestie at velit. Donec tincidunt tempus magna, quis facilisis libero elementum non. Sed velit lacus, laoreet sed augue fermentum, sagittis convallis metus. Sed nec est at massa venenatis aliquet. Donec pretium interdum fringilla. Sed ornare tellus enim, a tincidunt libero dictum vitae. Proin bibendum posuere dui. Donec sagittis neque massa, sed semper nulla vehicula at.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "FE",
+		"img" : ?
+	},
+	{
+		"id" : 8,
+		"title" : "Introduction to Spring Boot",
+		"author" : "Luv, Fatma",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tortor, condimentum eget sapien ac, dapibus varius ligula. Maecenas justo erat, semper sed nunc vel, vulputate eleifend dui. Integer id ipsum vitae nisi malesuada feugiat. Proin sit amet quam laoreet, feugiat mi vitae, vestibulum dui. Aliquam erat volutpat. Etiam hendrerit erat nec mi auctor elementum. Curabitur vestibulum lectus a ante tempor tincidunt et sed orci. Proin maximus tortor in risus auctor efficitur. Phasellus quam mauris, laoreet et feugiat ac, imperdiet at quam. Nullam sollicitudin nec diam vel finibus.",
+		"copies" : 10,
+		"copies_available" : 10,
+		"category" : "BE",
+		"img" : ?
+	},
+	{
+		"id" : 9,
+		"title" : "Become a Guru in React.js",
+		"author" : "Luv, Andrey",
+		"description" : "Pellentesque varius aliquam lacus quis rhoncus. Nam a dui lectus. Vestibulum libero elit, ultricies sit amet sagittis eu, molestie at velit. Donec tincidunt tempus magna, quis facilisis libero elementum non. Sed velit lacus, laoreet sed augue fermentum, sagittis convallis metus. Sed nec est at massa venenatis aliquet. Donec pretium interdum fringilla. Sed ornare tellus enim, a tincidunt libero dictum vitae. Proin bibendum posuere dui. Donec sagittis neque massa, sed semper nulla vehicula at.",
+		"copies" : 15,
+		"copies_available" : 15,
+		"category" : "FE",
+		"img" : ?
+	},
+	{
+		"id" : 10,
+		"title" : "Beginners Guide to Data Science",
+		"author" : "Luv, Ajay",
+		"description" : "Proin at urna faucibus, pretium mi in, dapibus metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam elementum ligula ut vestibulum tincidunt. Aenean dignissim enim eu libero accumsan, ultricies efficitur mauris bibendum. Sed vel blandit ligula, non tincidunt velit. Suspendisse vulputate tristique enim, a vulputate arcu accumsan vitae. Donec egestas ornare velit vestibulum tincidunt. Suspendisse ac dui nec tellus vulputate aliquet. Curabitur vitae mauris in turpis vulputate hendrerit at eget urna. Donec at suscipit nisi. Ut sit amet dignissim orci. Nunc sit amet tristique enim. Phasellus consectetur auctor dignissim. Curabitur cursus turpis dapibus, molestie dui finibus, molestie ante. Proin quis justo ut ipsum aliquet venenatis in ut enim.",
+		"copies" : 10,
+		"copies_available" : 10,
+		"category" : "Data",
+		"img" : ?
+	},
+	{
+		"id" : 11,
+		"title" : "Advanced Techniques in Java",
+		"author" : "Luv, Anna",
+		"description" : "Nunc eget lorem ac neque tincidunt mollis. Fusce finibus laoreet nunc nec hendrerit. Curabitur eu placerat urna, sit amet pellentesque enim. Donec velit ligula, congue eu lobortis vel, interdum nec tellus. Nulla nisl ipsum, porta non egestas sed, vulputate quis nisi. Etiam pellentesque in velit non convallis. Nullam id risus quis augue posuere sodales vel maximus justo. Maecenas nec leo a nibh aliquet placerat nec sed massa. Duis sit amet nisi libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non viverra dolor. Pellentesque ligula mauris, congue quis neque quis, mollis scelerisque ligula. Pellentesque semper, erat commodo luctus mollis, nulla ipsum consectetur dolor, quis blandit massa sem fringilla libero. Maecenas eget mi nec est condimentum fermentum. Vivamus vehicula est sit amet ante gravida, eu finibus quam elementum. Proin egestas leo eu sagittis euismod.",
+		"copies" : 12,
+		"copies_available" : 12,
+		"category" : "BE",
+		"img" : ?
+	},
+	{
+		"id" : 12,
+		"title" : "The Expert Guide to SQL",
+		"author" : "Luv, Poornima",
+		"description" : "Morbi eu tempus eros, in imperdiet sem. Nulla sed sagittis nisl, porttitor fringilla libero. Nullam ut urna aliquet, hendrerit quam in, dignissim diam. In in nibh vel nisi fermentum pretium sit amet vitae mi. Pellentesque eget augue efficitur, volutpat tellus eget, fringilla augue. Pellentesque tempus mi ac risus lacinia, et tincidunt lectus rutrum. Nullam et nibh a odio luctus tincidunt nec in ipsum. Sed ac est nulla. Nulla purus turpis, dignissim sit amet euismod lobortis, consequat ut dui. Maecenas commodo velit in elementum placerat. Nam sit amet blandit ante, sit amet mollis neque. Ut placerat venenatis leo sit amet dapibus. Nunc varius cursus lobortis. Aenean euismod dui at diam euismod aliquet. Fusce feugiat orci nec commodo placerat.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "Data",
+		"img" : ?
+	},
+	{
+		"id" : 13,
+		"title" : "Exploring DevOps",
+		"author" : "Luv, Mariana",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tortor, condimentum eget sapien ac, dapibus varius ligula. Maecenas justo erat, semper sed nunc vel, vulputate eleifend dui. Integer id ipsum vitae nisi malesuada feugiat. Proin sit amet quam laoreet, feugiat mi vitae, vestibulum dui. Aliquam erat volutpat. Etiam hendrerit erat nec mi auctor elementum. Curabitur vestibulum lectus a ante tempor tincidunt et sed orci. Proin maximus tortor in risus auctor efficitur. Phasellus quam mauris, laoreet et feugiat ac, imperdiet at quam. Nullam sollicitudin nec diam vel finibus.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "DevOps",
+		"img" : ?
+	},
+	{
+		"id" : 14,
+		"title" : "Introduction to SQL",
+		"author" : "Luv, Hugo",
+		"description" : "Pellentesque varius aliquam lacus quis rhoncus. Nam a dui lectus. Vestibulum libero elit, ultricies sit amet sagittis eu, molestie at velit. Donec tincidunt tempus magna, quis facilisis libero elementum non. Sed velit lacus, laoreet sed augue fermentum, sagittis convallis metus. Sed nec est at massa venenatis aliquet. Donec pretium interdum fringilla. Sed ornare tellus enim, a tincidunt libero dictum vitae. Proin bibendum posuere dui. Donec sagittis neque massa, sed semper nulla vehicula at.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "Data",
+		"img" : ?
+	},
+	{
+		"id" : 15,
+		"title" : "The Expert Guide to JavaScript",
+		"author" : "Luv, Elena",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tortor, condimentum eget sapien ac, dapibus varius ligula. Maecenas justo erat, semper sed nunc vel, vulputate eleifend dui. Integer id ipsum vitae nisi malesuada feugiat. Proin sit amet quam laoreet, feugiat mi vitae, vestibulum dui. Aliquam erat volutpat. Etiam hendrerit erat nec mi auctor elementum. Curabitur vestibulum lectus a ante tempor tincidunt et sed orci. Proin maximus tortor in risus auctor efficitur. Phasellus quam mauris, laoreet et feugiat ac, imperdiet at quam. Nullam sollicitudin nec diam vel finibus.",
+		"copies" : 10,
+		"copies_available" : 10,
+		"category" : "FE",
+		"img" : ?
+	},
+	{
+		"id" : 16,
+		"title" : "Exploring React.js",
+		"author" : "Luv, Filip",
+		"description" : "Pellentesque varius aliquam lacus quis rhoncus. Nam a dui lectus. Vestibulum libero elit, ultricies sit amet sagittis eu, molestie at velit. Donec tincidunt tempus magna, quis facilisis libero elementum non. Sed velit lacus, laoreet sed augue fermentum, sagittis convallis metus. Sed nec est at massa venenatis aliquet. Donec pretium interdum fringilla. Sed ornare tellus enim, a tincidunt libero dictum vitae. Proin bibendum posuere dui. Donec sagittis neque massa, sed semper nulla vehicula at.",
+		"copies" : 15,
+		"copies_available" : 15,
+		"category" : "FE",
+		"img" : ?
+	},
+	{
+		"id" : 17,
+		"title" : "Advanced Techniques in React.js",
+		"author" : "Luv, Mary",
+		"description" : "Proin at urna faucibus, pretium mi in, dapibus metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam elementum ligula ut vestibulum tincidunt. Aenean dignissim enim eu libero accumsan, ultricies efficitur mauris bibendum. Sed vel blandit ligula, non tincidunt velit. Suspendisse vulputate tristique enim, a vulputate arcu accumsan vitae. Donec egestas ornare velit vestibulum tincidunt. Suspendisse ac dui nec tellus vulputate aliquet. Curabitur vitae mauris in turpis vulputate hendrerit at eget urna. Donec at suscipit nisi. Ut sit amet dignissim orci. Nunc sit amet tristique enim. Phasellus consectetur auctor dignissim. Curabitur cursus turpis dapibus, molestie dui finibus, molestie ante. Proin quis justo ut ipsum aliquet venenatis in ut enim.",
+		"copies" : 10,
+		"copies_available" : 10,
+		"category" : "FE",
+		"img" : ?
+	},
+	{
+		"id" : 18,
+		"title" : "Introduction to C#",
+		"author" : "Luv, Bill",
+		"description" : "Nunc eget lorem ac neque tincidunt mollis. Fusce finibus laoreet nunc nec hendrerit. Curabitur eu placerat urna, sit amet pellentesque enim. Donec velit ligula, congue eu lobortis vel, interdum nec tellus. Nulla nisl ipsum, porta non egestas sed, vulputate quis nisi. Etiam pellentesque in velit non convallis. Nullam id risus quis augue posuere sodales vel maximus justo. Maecenas nec leo a nibh aliquet placerat nec sed massa. Duis sit amet nisi libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non viverra dolor. Pellentesque ligula mauris, congue quis neque quis, mollis scelerisque ligula. Pellentesque semper, erat commodo luctus mollis, nulla ipsum consectetur dolor, quis blandit massa sem fringilla libero. Maecenas eget mi nec est condimentum fermentum. Vivamus vehicula est sit amet ante gravida, eu finibus quam elementum. Proin egestas leo eu sagittis euismod.",
+		"copies" : 12,
+		"copies_available" : 12,
+		"category" : "BE",
+		"img" : ?
+	},
+	{
+		"id" : 19,
+		"title" : "Crash Course in JavaScript",
+		"author" : "Luv, Frank",
+		"description" : "Morbi eu tempus eros, in imperdiet sem. Nulla sed sagittis nisl, porttitor fringilla libero. Nullam ut urna aliquet, hendrerit quam in, dignissim diam. In in nibh vel nisi fermentum pretium sit amet vitae mi. Pellentesque eget augue efficitur, volutpat tellus eget, fringilla augue. Pellentesque tempus mi ac risus lacinia, et tincidunt lectus rutrum. Nullam et nibh a odio luctus tincidunt nec in ipsum. Sed ac est nulla. Nulla purus turpis, dignissim sit amet euismod lobortis, consequat ut dui. Maecenas commodo velit in elementum placerat. Nam sit amet blandit ante, sit amet mollis neque. Ut placerat venenatis leo sit amet dapibus. Nunc varius cursus lobortis. Aenean euismod dui at diam euismod aliquet. Fusce feugiat orci nec commodo placerat.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "FE",
+		"img" : ?
+	},
+	{
+		"id" : 20,
+		"title" : "Introduction to Machine Learning",
+		"author" : "Luv, Camila",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tortor, condimentum eget sapien ac, dapibus varius ligula. Maecenas justo erat, semper sed nunc vel, vulputate eleifend dui. Integer id ipsum vitae nisi malesuada feugiat. Proin sit amet quam laoreet, feugiat mi vitae, vestibulum dui. Aliquam erat volutpat. Etiam hendrerit erat nec mi auctor elementum. Curabitur vestibulum lectus a ante tempor tincidunt et sed orci. Proin maximus tortor in risus auctor efficitur. Phasellus quam mauris, laoreet et feugiat ac, imperdiet at quam. Nullam sollicitudin nec diam vel finibus.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "Data",
+		"img" : ?
+	},
+	{
+		"id" : 21,
+		"title" : "Become a Guru in Java",
+		"author" : "Luv, Priya",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tortor, condimentum eget sapien ac, dapibus varius ligula. Maecenas justo erat, semper sed nunc vel, vulputate eleifend dui. Integer id ipsum vitae nisi malesuada feugiat. Proin sit amet quam laoreet, feugiat mi vitae, vestibulum dui. Aliquam erat volutpat. Etiam hendrerit erat nec mi auctor elementum. Curabitur vestibulum lectus a ante tempor tincidunt et sed orci. Proin maximus tortor in risus auctor efficitur. Phasellus quam mauris, laoreet et feugiat ac, imperdiet at quam. Nullam sollicitudin nec diam vel finibus.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "BE",
+		"img" : ?
+	},
+	{
+		"id" : 22,
+		"title" : "Introduction to Python",
+		"author" : "Luv, Juan",
+		"description" : "Pellentesque varius aliquam lacus quis rhoncus. Nam a dui lectus. Vestibulum libero elit, ultricies sit amet sagittis eu, molestie at velit. Donec tincidunt tempus magna, quis facilisis libero elementum non. Sed velit lacus, laoreet sed augue fermentum, sagittis convallis metus. Sed nec est at massa venenatis aliquet. Donec pretium interdum fringilla. Sed ornare tellus enim, a tincidunt libero dictum vitae. Proin bibendum posuere dui. Donec sagittis neque massa, sed semper nulla vehicula at.",
+		"copies" : 20,
+		"copies_available" : 20,
+		"category" : "BE",
+		"img" : ?
+	}
+]
